ce63f1c42db7041fe117a122d1995b291e97b748
Anita Lipsky <anita@purplebugs.com>
2023-01-15 19:37:36 +0100

    test subscribe

--- sub.js
+++ sub.js
@@ -1,4 +1,15 @@
 const Redis = require("ioredis");
 const redis = new Redis(6379, "192.168.1.115");
 
-console.log(redis);
+// console.log(redis); // test connection
+
+redis.subscribe("Coco", (err, count) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log(`Subscribing to ${count} number of channels`);
+});
+
+redis.on("message", (channel, message) => {
+  console.log(`Getting message(s): ${message} on channel(s): ${channel}`);
+});
