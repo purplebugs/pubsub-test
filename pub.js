b69a3bfda640df1e97a5586904a848e2e1285812
Anita Lipsky <anita@purplebugs.com>
2023-01-15 21:09:46 +0100

    Publish every 5 seconds

--- pub.js
+++ pub.js
@@ -3,3 +3,10 @@ const redis = new Redis(); // To use local redis if run `redis-server` locally
 
 redis.publish("Coco", JSON.stringify({ msg: "Coco loves hot chocolate" }));
 redis.publish("Cherry", JSON.stringify({ msg: "Cherry likes 80s music" }));
+
+setInterval(() => {
+  redis.publish(
+    "Coco",
+    JSON.stringify({ msg: "The time is now", time: Date.now() })
+  );
+}, 5000); // Publish message every 5 seconds
